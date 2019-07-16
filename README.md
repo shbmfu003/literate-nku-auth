be6519e5e2b97fce34644baf6757f842e03016e9
Mfundo Shabalala <mfundoshabalala@gmail.com>
2019-07-16 23:44:19 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# literate-nku-auth" 
---
+++ app.js
@@ -0,0 +1,88 @@
+// Express Application
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+// Connect To Database
+mongoose.connect('mongodb://localhost/nku', { useNewUrlParser: true });
+let db = mongoose.connection;
+
+// Check Connection
+db.once('open', function(){
+  console.log('Connected to MongoDB');
+});
+
+// Check for DB errors
+db.on('error', function(err){
+  console.log(err);
+});
+
+// Init App
+const app = express();
+
+//Bring in Models
+let Article = require('./models/article');
+
+// Load View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Body Parser Middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+// Set Public Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Home Route
+app.get('/', function(req, res){
+  Article.find({}, function(err, articles){
+    if(err){
+      console.log(err);
+    } else {
+      res.render('index', {
+        title:'Articles',
+        articles: articles
+      });
+    }
+  });
+});
+
+// Get Single Article
+app.get('/article/:id', function(req, res){
+  Article.findById(req.params.id, function(err, article){
+    res.render('article', {
+      title: 'Add Articles'});
+  })
+});
+
+// Add Route
+app.get('/articles/add', function(req, res){
+  res.render('add_article', {
+    title: 'Add Articles'
+  });
+});
+
+// Add Submit POST Route
+app.post('/articles/add', function(req, res){
+  let article = new Article();
+  article.title  = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+
+  article.save(function(err){
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect('/');
+    }
+  })
+});
+
+// Start Server
+app.listen(3000, function(){
+  console.log('Server started on port 3000...')
+});
